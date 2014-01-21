6f7325a133a5505f2e241b9f500d22e0310ccca1
laudon <laudon1@gmail.com>
2014-01-21 15:28:57 -0600

    Added a few features
    Profile now checks for these immunities - Cyclone, divine shield, ice
    block, touch of karma, materialize, and dispersion.
    
    Interrupts were added.
    
    Tranquilizing shot was fixed and a few spells were added.

---
+++ Probably_BM_Hunter_PvP_Zach/rotation.lua
@@ -0,0 +1,90 @@
+-- ProbablyEngine Rotation Packager
+-- Custom Beast Mastery Hunter Rotation
+-- Created on Jan 21st 2014 1:45 am
+ProbablyEngine.rotation.register_custom(253, "BM_Hunter_PvP", {
+
+--Keybinds
+ { "Scatter Shot", "modifier.control", "mouseover" }, 
+ { "Freezing trap", "modifier.alt", "ground" }, 
+--{ "Explosive Trap", { "modifier.", "ground" }}, 
+ { "Binding Shot", "modifier.lshift", "ground" },
+ { "Wyvern Sting", { "mouseover.exists", "modifier.lshift",}, "mouseover" },
+ { "Intimidation", "modifier.lshift" },
+-- { "Scare Beast", "modifier." }, 
+-- { "Ice Trap", "modifier." },
+  
+
+ 
+
+--tranq shot important stuff
+ { "Tranquilizing Shot", "target.buff(Hand of Protection).any"}, 
+ { "Tranquilizing Shot", "target.buff(Power Word: Shield).any"}, 
+ { "Tranquilizing Shot", "target.buff(Nature's Swiftness).any"}, 
+ { "Tranquilizing Shot", "target.buff(Icy Veins).any"}, 
+ { "Tranquilizing Shot", "target.buff(Heroism).any"}, 
+ { "Tranquilizing Shot", "target.buff(Bloodlust).any"}, 
+ { "Tranquilizing Shot", "target.buff(Ice Barrier).any"}, 
+ { "Tranquilizing Shot", "target.buff(Hand of Freedom).any"}, 
+  
+  --Interrupts
+ { "Counter Shot", {"target.casting", "!target.immune.silence", "modifier.interrupts" }},
+ { "Counter Shot", {"focus.casting", "!focus.immune.silence", "modifier.interrupts" }},
+
+--Cooldowns (Survivability)
+ { "Deterrence", {"player.health <= 25", "!player.buff(deterrence)" }}, --HP below 30%
+ { "Exhilaration", "player. health <=45"}, 
+ { "Exhilaration", "pet. health <=25"},
+ { "Master's Call", "player.state.root"},
+ { "Master's Call", "player.state.snare"},
+ { "#5512", "player.health <= 50" }, --Healthstone
+ --Pet survival
+ { "Mend Pet", {"pet.health <= 90", "!pet.buff{Mend Pet}"}},
+
+ --Check if target is immune
+ {
+
+ 	{
+ --Cooldowns (DPS)
+ { "#gloves" },
+ { "#trinket1", "modifier.cooldowns" },
+ { "Berserking" },
+ { "Blood Fury" },
+ { "Beastial Wrath" },
+ { "Rabid" },
+ { "Stampede", "modifier.cooldowns" },
+ { "Rapid Fire", { "modifier.cooldowns", "player.focus <= 110" }}, 
+ { "Fervor", "player.focus <= 50"}, 
+ { "Dire Beast"},
+ 
+  --Start Rotation
+ { "Counter Shot", {"target.health < 40", "target.casting"}}, --auto interrupt if target is below 40%
+ { "Kill Shot", "target.health <= 20" }, 
+ { "Concussive Shot", "target.moving"},
+ { "A Murder of Crows" },
+ { "Lynx Rush" },
+ --{ "Widow Venom", "!target.debuff(Widow Venom)" },
+ { "Serpent Sting", {"!target.debuff(Serpent Sting)", "target.health > 40" }}, 
+ { "Focus Fire", {"!player.buff(Beastial Wrath)", "player.buff(Frenzy).count = 5"}},
+ { "Kill Command"},
+ { "Glaive Toss" },
+ { "Arcane Shot", {"player.focus >= 60", "!player.spell(A Murder of Crows).usable"}}, 
+ { "Arcane Shot", {"player.focus >= 30", "player.buff(Thrill of the Hunt)", "!player.spell(A Murder of Crows).usable"}},
+ { "Cobra Shot" }
+ 	},
+
+ 		{								 
+			"!target.buff(Ice Block).any",
+			"!target.debuff(Cyclone).any",
+			"!target.buff(Divine Shield)",
+			"!target.buff(Touch of Karma).any", 
+			"!target.buff(Materialize).any",
+			"!target.buff(Dispersion).any",
+   		 }
+    },
+ }, 
+--Out of combat 
+{
+{"Hunter's Mark", {"target.exists", "!target.debuff(Hunter's Mark)"}, "target" }
+--{"Aspect of the Cheetah", {"player.moving", "!player.buff(Aspect of the Cheetah)"}}
+}
+)
